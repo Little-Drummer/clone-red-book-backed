ac7088040e6ef8e8a4f45317768593d77ad7e9d6
YJXW <1085561450@qq.com>
2024-02-07 13:24:27 +0800

    项目初始化

---
+++ src/main/java/com/yjxw/CloneRedBookBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.yjxw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloneRedBookBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloneRedBookBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yjxw/CloneRedBookBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yjxw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloneRedBookBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
