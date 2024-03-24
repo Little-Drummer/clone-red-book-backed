65adcb63f3dd3136a76ab2e25812d4be97be0b12
YJXW <1085561450@qq.com>
2024-03-24 16:33:20 +0800

    测试jenkins

--- src/main/java/com/yjxw/CloneRedBookBackEndApplication.java
+++ src/main/java/com/yjxw/CloneRedBookBackEndApplication.java
@@ -1,14 +1,17 @@
 package com.yjxw;
 
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @MapperScan("com.yjxw.mapper")
+@Slf4j
 public class CloneRedBookBackEndApplication {
 
     public static void main(String[] args) {
+        log.info("项目开始启动...");
         SpringApplication.run(CloneRedBookBackEndApplication.class, args);
     }
 
